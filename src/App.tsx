5f98aa892cd8009380fe067174743effdfe153b3
Thay Giao Tien <thelaststanding97@gmail.com>
2021-06-04 15:36:49 +0300

    v1.3: add justifyContent and alignItems properties into the FuntionComponent

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,19 @@ import Grid from "./components/Grid/Grid";
 import GridRuler from "./components/GridRuler/GridRule";
 function App() {
   return (
-    <div className="App" style={{margin:"16px", position:"relative"}}  >
+    <div className="App" style={{margin:"16px", position:"relative", height: "100vh"}}  >
       <GridRuler spacing="lg"></GridRuler>
-      <Grid container spacing="sm">
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+      <Grid container spacing="sm" justifyContent="center" alignItems="flex-end" style={{height: "100%"}}>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box1</Box> 
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box2</Box> 
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box3</Box> 
         </Grid> 
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box4</Box> 
         </Grid>
       </Grid>
--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -4,6 +4,8 @@ import cn from "classnames";
 
 type Cols=1|2|3|4|5|6|7|8|9|10|11|12;
 type Spacing= "sm"|"md"|"lg";
+type justifyContent="center"|"flex-start"|"flex-end"|"space-between";
+type alignItems="center"|"flex-start"|"flex-end";
 const Grid:FunctionComponent<{
     children: ReactNode,
     container?:boolean,                            // ?: optional props
@@ -13,17 +15,22 @@ const Grid:FunctionComponent<{
     md?: Cols,
     lg?: Cols,
     spacing?: Spacing;
-}>=({children, container, item, xs, sm, md, lg, spacing})=>{
+    justifyContent?: justifyContent;
+    alignItems?: alignItems;
+} & React.HTMLAttributes<HTMLDivElement>                        // tell typescript that ...props now to be passed in the div
+  >=({children, container, item, xs, sm, md, lg, spacing, justifyContent, alignItems, ...props})=>{ 
     const classNames=cn({
-        [styles.Grid_container]: container,
+        [styles.Grid_container]: container,   
         [styles.Grid_item]:item,
         [styles[`Grid_xs_${xs}`]]: xs,
         [styles[`Grid_sm_${sm}`]]: sm,
         [styles[`Grid_md_${md}`]]: md,
         [styles[`Grid_lg_${lg}`]]: lg, 
         [styles[`Grid_spacing_${spacing}`]]: spacing,
+        [styles[`Grid_justifyContent_${justifyContent}`]]: justifyContent,
+        [styles[`Grid_alignItems_${alignItems}`]]: alignItems,
     });
-    return <div className={classNames}>{children}</div>;
+    return <div className={classNames} {...props}>{children}</div>;
 }
 export default Grid;
 
